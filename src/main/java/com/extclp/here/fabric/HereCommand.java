67887d4cc8ea53b027ce74bf0a9fe390fed41f0a
extclp <2842417407@qq.com>
2020-05-29 20:57:09 +0800

    first

---
+++ src/main/java/com/extclp/here/fabric/HereCommand.java
@@ -0,0 +1,73 @@
+package com.extclp.here.fabric;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.math.BlockPos;
+
+public class HereCommand {
+
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher)  {
+        dispatcher.register(CommandManager.literal("here")
+                .requires(source -> source.getEntity() instanceof ServerPlayerEntity &&
+                        CarpetHook.canUseHereCommand(source))
+                .executes(context -> glowing(context.getSource())));
+        dispatcher.register(CommandManager.literal("glowing")
+                .requires(CarpetHook::canUseGlowingCommand)
+                .executes(context -> glowing(context.getSource().getPlayer()))
+                .then(CommandManager.literal("clear")
+                        .executes(context -> clearGlowing(context.getSource().getPlayer())))
+                .then(CommandManager.argument("glowing_time", IntegerArgumentType.integer(1, 86400))
+                        .executes(context -> glowing(context.getSource().getPlayer(),
+                                IntegerArgumentType.getInteger(context, "glowing_time"))))
+        );
+    }
+
+    private static String getWorldDisplayName(ServerWorld world){
+        int dimensionID = world.getDimension().getType().getRawId();
+        String displayName = HereMod.getConfig().dimensions_name.get(dimensionID);
+        if(displayName != null){
+            return displayName;
+        }else {
+            return Integer.toString(dimensionID);
+        }
+    }
+
+    private static int glowing(ServerCommandSource source) throws CommandSyntaxException {
+        ServerPlayerEntity player = source.getPlayer();
+        BlockPos blockPos = player.getBlockPos();
+        source.getMinecraftServer().getPlayerManager().broadcastChatMessage(
+                new TranslatableText(HereMod.getConfig().broadcast_message, player.getDisplayName(),
+                        getWorldDisplayName(source.getWorld()),
+                        blockPos.getX(), blockPos.getY(), blockPos.getZ(),
+                        player.getServerWorld().getDimension().getType().getRawId()
+                ), true);
+        return glowing(player);
+    }
+
+    private static int glowing(ServerPlayerEntity player) {
+        return glowing(player, HereMod.getConfig().glowing_time);
+    }
+
+    private static int glowing(ServerPlayerEntity player, int glowing_time){
+        if(glowing_time > 0){
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, glowing_time * 20));
+            player.sendMessage(new TranslatableText(HereMod.getConfig().glowing_message, glowing_time));
+        }
+        return 1;
+    }
+
+    private static int clearGlowing(ServerPlayerEntity player){
+        player.removeStatusEffect(StatusEffects.GLOWING);
+        player.sendMessage(new LiteralText(HereMod.getConfig().glowing_effect_removed_message));
+        return 1;
+    }
+}
